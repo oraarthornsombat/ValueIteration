21ee4eae82b575e8a9c44d7303e5d230ffb87ae0
oraarthornsombat <oraarthornsombat@users.noreply.github.com>
2016-04-04 19:29:19 -0400

    Finished wind cases!

--- src/ValueIteration.java
+++ src/ValueIteration.java
@@ -15,8 +15,8 @@ public class ValueIteration {
 
 	public static double getMaxValue(int i, int j, int wind) {
 
-		if (wind == 0) {
-			double max = Double.NEGATIVE_INFINITY;
+		double max = Double.NEGATIVE_INFINITY;
+		if (wind == 0 || j < 3 || j > 5) {
 			// check N
 			if (i > 0) {
 				if (oldGrid[i - 1][j] > max) {
@@ -69,17 +69,99 @@ public class ValueIteration {
 			if (oldGrid[i][j] > max) {
 				max = oldGrid[i][j];
 			}
-			return max;
-		} else if (wind == 1) {
-			// case 1
-			double max = Double.NEGATIVE_INFINITY;
-			return max;
+		} else if ((j > 2 || j < 6) && wind == 1) {
 
-		} else {
-			// case 2
-			double max = Double.NEGATIVE_INFINITY;
-			return max;
+			if (i > 1) {
+				// check N
+				if (oldGrid[i - 2][j] > max) {
+					max = oldGrid[i - 2][j];
+				}
+				// check NE
+				if (oldGrid[i - 2][j + 1] > max) {
+					max = oldGrid[i - 2][j + 1];
+				}
+				// check NW
+				if (oldGrid[i - 2][j - 1] > max) {
+					max = oldGrid[i - 2][j - 1];
+				}
+			}
+			if (i > 0) {
+				// check E
+				if (oldGrid[i - 1][j + 1] > max) {
+					max = oldGrid[i - 1][j + 1];
+				}
+				// check W
+				if (oldGrid[i - 1][j - 1] > max) {
+					max = oldGrid[i - 1][j - 1];
+				}
+				// check current
+				if (oldGrid[i - 1][j] > max) {
+					max = oldGrid[i - 1][j];
+				}
+
+			}
+			// check SE
+			if (oldGrid[i][j + 1] > max) {
+				max = oldGrid[i][j + 1];
+			}
+			// check S
+			if (oldGrid[i][j] > max) {
+				max = oldGrid[i][j];
+			}
+			// check SW
+			if (oldGrid[i][j - 1] > max) {
+				max = oldGrid[i][j - 1];
+			}
+
+		} else if ((j > 2 || j < 6) && wind == 2) {
+
+			if (i > 2) {
+				// check N
+				if (oldGrid[i - 3][j] > max) {
+					max = oldGrid[i - 3][j];
+				}
+				// check NE
+				if (oldGrid[i - 3][j + 1] > max) {
+					max = oldGrid[i - 3][j + 1];
+				}
+				// check NW
+				if (oldGrid[i - 3][j - 1] > max) {
+					max = oldGrid[i - 3][j - 1];
+				}
+			}
+			if (i > 1) {
+				// check E
+				if (oldGrid[i - 2][j + 1] > max) {
+					max = oldGrid[i - 2][j + 1];
+				}
+				// check W
+				if (oldGrid[i - 2][j - 1] > max) {
+					max = oldGrid[i - 2][j - 1];
+				}
+				// check current
+				if (oldGrid[i - 2][j] > max) {
+					max = oldGrid[i - 2][j];
+				}
+
+			}
+			if (i > 0) {
+				// check SE
+				if (oldGrid[i - 1][j + 1] > max) {
+					max = oldGrid[i - 1][j + 1];
+				}
+				// check S
+				if (oldGrid[i - 1][j] > max) {
+					max = oldGrid[i - 1][j];
+				}
+				// check SW
+				if (oldGrid[i - 1][j - 1] > max) {
+					max = oldGrid[i - 1][j - 1];
+				}
+			}
 		}
+
+		return max;
+
 	}
 
 	public static void findValueFunction(int wind) {
@@ -113,6 +195,7 @@ public class ValueIteration {
 	}
 
 	public static void main(String[] args) {
+		//input wind=0 for Case 1 (no wind), wind=1 for Case 2 (light wind), and wind=2 for Case 3 (strong wind)
 		int wind = 0;
 		findValueFunction(wind);
 		for (int i = 0; i < 7; i++) {
